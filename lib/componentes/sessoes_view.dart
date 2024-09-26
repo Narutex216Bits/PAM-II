8909dff848fcf477f098eb4350004903bbccc18f
Narutex216Bits <djxan@yahoo.com.br>
2024-09-25 22:38:13 -0300

    branch com o código da aula do dia Wed, Sep 25, 2024 10:38:13 PM

---
+++ lib/componentes/sessoes_view.dart
@@ -0,0 +1,32 @@
+import 'package:cinemeu/entidade/sessao.dart';
+import 'package:flutter/material.dart';
+
+class SessoesView extends StatelessWidget {
+  final Map<String, List<Sessao>> sessoes;
+
+  const SessoesView({
+    super.key,
+    required this.sessoes,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: sessoes.length,
+        itemBuilder: (context, index) {
+          final data = sessoes.keys.elementAt(index);
+          final items = sessoes.values.elementAt(index);
+
+          return ExpansionTile(
+            title: Text(sessoes.keys.elementAt(index)),
+            children: const [
+              Text("A"),
+              Text("B"),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
